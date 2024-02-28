226a18b29c14ee937dc6cde8e68d6ba7192f15e2
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2024-02-28 03:49:11 -0800

    first commit

---
+++ app/index.js
@@ -0,0 +1,26 @@
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  PopularJobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+const Home = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
